f87bae39cc5a4c000afadda74bb19dcc19287ad3
drambique <drambique@users.noreply.github.com>
2018-02-14 23:23:19 -0500

    Heartbeast tutorial part 3
    - smoother character movement using acceleration and lerp
    - fall animation
    - player now has capsule collision shape instead of box

--- Heartbeast Mini Platformer Tutorial/KinematicBody2D.gd
+++ Heartbeast Mini Platformer Tutorial/KinematicBody2D.gd
@@ -2,31 +2,40 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -500
 var motion = Vector2()
 
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
 		$Sprite.play("Idle")
+		friction = true
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2) # Slow down by 20% every frame
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05) # Slow down by 5% every frame
 	
 	motion = move_and_slide(motion, UP)
 	
